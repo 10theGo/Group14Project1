a2cae10144d1538aea23cae9a7b653b36bf26584
Evelin <evelindacanovas@gmail.com>
2021-06-17 01:46:35 +0200

    test2 enter input in input box

--- src/test/java/TC2_Evelin.java
+++ src/test/java/TC2_Evelin.java
@@ -1,18 +1,29 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TC2_Evelin {
 
+ WebDriver driver;
+
  @Test
-    public void getDriver(){
+    public void getDriver() throws InterruptedException {
 
      WebDriverManager.chromedriver().setup();
 
-     WebDriver driver =  new ChromeDriver();
+   driver =  new ChromeDriver();
 
      driver.navigate().to("https://www.etsy.com/");
+ }
+ @Test
+ public void input(){
+
+  WebElement searchBox = driver.findElement(By.id("global-enhancements-search-query"));
+  searchBox.sendKeys("dada atolye");
 
  }
+
 }
