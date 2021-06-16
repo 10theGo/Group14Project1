8a079bc2940a33c3f5af2fa10c84f7845ded1664
Evelin <evelindacanovas@gmail.com>
2021-06-17 00:10:12 +0200

    test1 navigate to etsy done

--- src/test/java/TC2_Evelin.java
+++ src/test/java/TC2_Evelin.java
@@ -1,4 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TC2_Evelin {
 
+ @Test
+    public void getDriver(){
 
+     WebDriverManager.chromedriver().setup();
+
+     WebDriver driver =  new ChromeDriver();
+
+     driver.navigate().to("https://www.etsy.com/");
+
+ }
 }
