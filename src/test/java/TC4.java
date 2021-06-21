deaad8b7ca95548f1e2db1c5d89276838aca09e4
onurcan-ada <sleepiaapp@gmail.com>
2021-06-21 16:33:37 +0300

    TC4 created in new branch

---
+++ src/test/java/TC4.java
@@ -0,0 +1,11 @@
+public class TC4 {
+
+    //Just created new branch for TC4
+}
+//TC4: etsy.com Verification – Section 4
+//1. Open Firefox browser
+//2. Go to https://www.etsy.com/
+//3. Click Clothing & Shoes module
+//4. Print total result. (All categories  Clothing & Shoes (10,137,399 results, with Ads Learn more))
+//5. Then click  Dining & Serving link on the main page.
+//6. Verify total result number is less than previous page's total result.
