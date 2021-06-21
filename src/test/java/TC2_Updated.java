c73dc5efc76476ffd0d755bbe5404f1b4524fee1
Evelin <evelindacanovas@gmail.com>
2021-06-22 00:26:14 +0200

    Navigate to the etsy page Search for item enter to results

--- src/test/java/TC2_Updated.java
+++ src/test/java/TC2_Updated.java
@@ -1,2 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
 public class TC2_Updated {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+      driver = WebDriverFactory.getDriver("chrome");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+       // driver.quit();
+    }
+    @Test
+    public void bohemianSandalsItem() throws InterruptedException {
+        driver.get("https://www.etsy.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@class='wt-btn wt-btn--filled wt-mb-xs-0']")).click();
+
+        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("bohemian sandals");
+
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+    }
+
 }
