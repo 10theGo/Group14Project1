489d422b53b408f62b4ee9c32fa61eb8a70bfe14
Evelin <evelindacanovas@gmail.com>
2021-06-23 00:33:53 +0200

    Working on relevant button

--- src/test/java/TC2_Updated.java
+++ src/test/java/TC2_Updated.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
+
+import java.util.List;
 
 public class TC2_Updated {
 
@@ -28,6 +31,21 @@ public class TC2_Updated {
         driver.findElement(By.id("global-enhancements-search-query")).sendKeys("bohemian sandals");
 
         driver.findElement(By.xpath("//*[@type='submit']")).click();
+
+        //Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//*[@title='Ordenar por: Relevancia']")).click();
+
+        List<WebElement> relevantElements = driver.findElements(By.xpath("//*[@role='menuitemradio']"));
+        for (WebElement relevantElement : relevantElements) {
+            System.out.println(relevantElement.getText());
+        }
+        WebElement relevantItem = driver.findElement(By.xpath("//*[@data-sort-by='most_relevant']"));
+       // relevantItem.click();
+
+       Assert.assertTrue(relevantItem.isDisplayed(),"verify options relevant item");
+       Thread.sleep(3000);
+       //Assert.assertTrue(relevantItem.isSelected(), "verify is selected");
     }
 
 }
