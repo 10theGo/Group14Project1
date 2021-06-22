73a581dfe163916f386a699696a6f42be5359791
AdnanRumi <mugterib@gmail.com>
2021-06-22 15:41:29 +0200

    Test3 initial test commit

---
+++ src/test/java/TC_03_AY.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_03_AY {
+
+    @Test
+    public void test1() {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.etsy.com/");
+
+
+
+
+
+    }
+}
