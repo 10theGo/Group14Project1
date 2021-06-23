132058e3ee9120c9a034f0d4c04a5bba340ba2e0
burhanalioglu <burhanalioglu@gmail.com>
2021-06-23 18:40:45 +0300

    Started to TC7

---
+++ src/test/java/TC7.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+
+public class TC7 {
+
+//    TC7: etsy.com Verification – Section 7
+//            1. Open Chrome browser
+//            2. Go to https://www.etsy.com/
+//            3. Print "bohem" in the search box.
+//            4. Click "bohemian sandals"' in the dropdown menu
+//            5. Click "All Filters" and select FREE shipping.
+//            6. Click Apply.
+//            7. Check the total result is 8.
+    WebDriver driver;
+
+}
